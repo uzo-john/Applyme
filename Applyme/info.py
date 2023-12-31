358ec438135b37f98b8d4fe572566fb1c6616048
johnpaul <109599980+uzo-john@users.noreply.github.com>
2023-12-31 15:14:31 -0800

    done

---
+++ Applyme/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'imtp.gmail.com'
+EMAIL_HOST_USER = 'johnpauluzowuru2018@gmail.com'
+EMAIL_HOST_PASSWORD = 'Johnpaul@1998'
+EMAIL_POST = 587
---
+++ authentication/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('signout', views.signout, name="signout"),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
